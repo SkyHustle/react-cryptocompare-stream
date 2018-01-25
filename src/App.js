f84b7029eb45f96a5e6043ebafe2ef644ddd798e
Dmitry Sky <dmitry.sky@icloud.com>
2018-01-25 10:49:09 -0700

    install react-bootstrap-table, react-bootstrap, socket.io, and bring in ccc-streamer-utilities for unpacking and formatting streamed data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Tabs, Tab } from 'react-bootstrap';
+import CCC from './ccc-streamer-utilities';
+
+// Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
+// Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
+// For aggregate quote updates use CCCAGG as market
+import io from 'socket.io-client';
+const socket = io.connect('https://streamer.cryptocompare.com/');
 
 class App extends Component {
   render() {
@@ -11,7 +20,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
